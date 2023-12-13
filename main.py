5a8047768863122bda79f083dcddc7096f618d72
Mayahdw <143128208+Mayahdw@users.noreply.github.com>
2023-12-12 21:38:12 -0600

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ if len(type)==0:
 if len(method)==0:
     print("*Method not found, default method : Dictionary*")
     setMethod("dictionary")
-if method == "bruteforce" and type is not "plaintext":
+if (method == "bruteforce") and not (type == "plaintext"):
     print("*Not possible, Brute Force can only work with Plain Text*")
     quit()
 
